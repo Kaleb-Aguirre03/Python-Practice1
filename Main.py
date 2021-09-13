1bb1fe491ba370d7bbffccc1c413b5c55087df42
Kaleb-Aguirre03 <820930@mystma.org>
2021-09-13 09:46:33 -0500

    Created Favorite Movies List

---
+++ Main.py
@@ -0,0 +1,17 @@
+#print title of list
+print("My Top 5 Movies:")
+
+#print 1st Movie
+print("1) Home Alone 2")
+
+#print 2nd Movie
+print("2) Spider-man: Into the Spider-verse")
+
+#print 3rd Movie
+print("3) Dragon Ball Super: Broly")
+
+#print 4th Movie
+print("4) Dragon Ball Z: Fusion Reborn")
+
+#print 5th Movie
+print("5) The Amazing Spider-Man 2")
