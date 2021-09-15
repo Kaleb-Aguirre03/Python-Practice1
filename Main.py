41d2069233486eebcf066812e4f76ee534ec135b
Kaleb-Aguirre03 <820930@mystma.org>
2021-09-15 09:12:59 -0500

    Defined more functions

--- Main.py
+++ Main.py
@@ -17,5 +17,48 @@ def topFiveMovies():
     #Print the 5th movie
     print("5) The Amazing Spider-Man 2")
 
+def topThreeSports():
+    #print the title of the list
+    print("My Top Three Sports:")
+
+    #print the 1st sport
+    print("1) Baseball")
+
+    #print the 2nd sport
+    print("2) American Football")
+
+    #print the 3rd sport
+    print("3) Basketball")
+
+def namesOfFam():
+    #print the title of the list
+    print("Names of My Family:")
+
+    #print the name of mom
+    print("Mom: Charity")
+
+    #print the name of dad
+    print("Dad: Matt")
+
+    #print the name of sis
+    print("Sister: Miki")
+
+    #print the name of dog
+    print("Dog: Teddy")
+
+def statesIveBeenTo():
+    #print title of the list
+    print("States That I Have Been To:")
+
+    #print 1st state
+    print("Wisconsin")
+
+    #print 2nd state
+    print("South Dakota")
+
+    #print 3rd state
+    print("Florida")
+
+
 #run the funtion topFiveMovies WOOOOOO
 topFiveMovies()
